4203c57e2b9bb7b565aff19c22ef299fd332d2f1
Juan Francisco Martínez Vera <juan.francisco.martinez@est.fib.upc.edu>
2017-01-19 17:25:00 +0100

    Added more options. Now you can generate instances with gathered or scattered locations.

--- scripts/InstanceGenerator.py
+++ scripts/InstanceGenerator.py
@@ -74,24 +74,7 @@ def calculeDistances2(ncities, poscities, stLocation):
 
     return distances
 
-def calculeTask(ncities, poscities):
-    # TODO: The task should be inversely proportional
-    # to the distance to the center of the plane.
-    
-    task=[]
-    for i in range(ncities):
-        if NICE:
-            dist_to_center=math.sqrt(poscities[i][0]**2 + \
-                                 poscities[i][1]**2)
-            # We know that the maximum distance is MAX_DISTANC/ncities
-            time=random.randrange(int(MAX_DISTANC/ncities)-int(dist_to_center))
-        else:
-            time=random.randrange(1,MAX_DISTANC)
-        task.append(time)
-        
-    return task
-
-def calculeTask2(ncities, distances, stLocation):
+def calculeTask(ncities, distances, stLocation):
     task=[]
     for i in range(ncities):
         from_st = distances[stLocation-1][i]
@@ -101,88 +84,66 @@ def calculeTask2(ncities, distances, stLocation):
         task.append(task_time)
     
     return task
-    
-def calculeWindows(ncities):
-    # TODO: To model
 
+def calculeWindows(ncities, distances, tasks, stLocation, maxwsize):
     minW=[]
     maxW=[]
-    for i in range(ncities):
-        if not NICE:
-            min_w=random.randrange(MAX_DISTANC)
-            max_w=random.randrange(min_w, MAX_DISTANC)
-        else:
-            min_w=1
-            max_w=MAX_DISTANC
-        
-        minW.append(min_w)
-        maxW.append(max_w)
-        
-    return minW, maxW
-
-def calculeWindows2(ncities, distances, tasks, stLocation):
-    minW=[]
-    maxW=[]
-    
-    WSIZE=500
     
     for i in range(ncities):
         if i == stLocation-1: continue
         
         from_st = distances[stLocation-1][i]
         #Purelly random
-        min_w = random.randrange(0, from_st)
-        max_w = random.randrange(from_st, 720)
+        #min_w = random.randrange(0, from_st)
+        #max_w = random.randrange(from_st, 720)
         
         # Random with max windows size
-        #min_w = random.randrange(max(0,from_st-WSIZE), from_st-1)
-        #max_w = random.randrange(from_st, min(720,from_st+WSIZE))
-        
-        # Exact windows size
-        #min_w = max(0,from_st-(WSIZE/2))
-        #max_w = min(720,from_st+WSIZE/2)
+        min_w = random.randrange(max(0,from_st-maxwsize), from_st-1)
+        max_w = random.randrange(from_st, min(720,from_st+maxwsize))
         
         maxW.append(max_w)
         minW.append(min_w)
-        
-        #maxW.append(720)
-        #minW.append(0)
         
     minW.insert(stLocation-1,0)
     maxW.insert(stLocation-1,720)
     return minW, maxW
     
 def main(argc, argv):
-    if argc>1:
-        ncities=int(argv[1])
-    else:
-        ncities=NUM_CITIES
-
-    # This calculus is enforcing that the maximum distance
-    # between two cities in the worst case (diagonal) 
-    # is at most 720/ncitis in order to ensure feasible solutions.
-    #SPACE_DIM=math.sqrt( (MAX_DISTANC/ncities)**2/2)
-    SPACE_DIM=math.sqrt( (MAX_DISTANC/2)**2/2)
+    if argc < 4:
+        print("Error on parameters.")
+        print("./{0} <nlocations> <maxwinsize> <SCAT | GATH>".format(argv[0]))
+        return(0)
+        
+    # Parsing params
+    ncities = int(argv[1])
+    maxwinsize = int(argv[2])
+    scattered = (argv[3] == "SCAT")
     
-    ###################
-    # Generating data #
-    ###################
+    # Generating data
+    SPACE_DIM=math.sqrt( (MAX_DISTANC/2)**2/2)
     stLocation = random.randrange(1,ncities)
     
-    #poscities = generatePoints(ncities, 2*SPACE_DIM)
-    poscities = generatePoints(ncities, SPACE_DIM)
-    #distances = calculeDistances2(ncities, poscities, stLocation)
-    distances = calculeDistances(ncities, poscities)
-    citytask  = calculeTask2(ncities, distances, stLocation)
-    minW, maxW  = calculeWindows2(ncities, distances, citytask, stLocation)
-    
+    if scattered == True:
+        poscities = generatePoints(ncities, 2*SPACE_DIM)
+        distances = calculeDistances2(ncities, poscities, stLocation)
+    else:
+        poscities = generatePoints(ncities, SPACE_DIM)
+        distances = calculeDistances(ncities, poscities)
+        
+    citytask  = calculeTask(ncities, distances, stLocation)
     citytask[stLocation-1]=0
+    minW, maxW  = calculeWindows(ncities, distances, citytask, stLocation, maxwinsize)
     
-    #import pdb; pdb.set_trace()
+    windows_sizes = []
+    windows_sum = 0
+    
+    for i in range(ncities): windows_sizes.append(maxW[i]-minW[i])
+    for w in windows_sizes: windows_sum += w
+        
+    windows_mean = windows_sum / ncities
 
-    ####################
-    # Writting results #
-    ####################
+    # Writting results
+    
     filename="./instance_{0}_{1}.dat".format(ncities, RAND_SEED)
     outfile=open(filename, "w")
     
@@ -197,15 +158,12 @@ def main(argc, argv):
     outfile.write("bigM=100000;\n\n")
     outfile.write("nLocations={0};\n".format(ncities))
     outfile.write("startLocation={0};\n".format(stLocation))
-    outfile.write("distances=")
-    pp.pprint(distances)
-    outfile.write(";\ntask=")
-    pp.pprint(citytask)
-    outfile.write(";\nminW=")
-    pp.pprint(minW)
-    outfile.write(";\nmaxW=")
-    pp.pprint(maxW)
+    outfile.write("distances="); pp.pprint(distances)
+    outfile.write(";\ntask="); pp.pprint(citytask)
+    outfile.write(";\nminW="); pp.pprint(minW)
+    outfile.write(";\nmaxW="); pp.pprint(maxW)
     outfile.write(";")
+    outfile.write("// Mean windows sizes: {0}".format(windows_mean))
     
     print ("{0} input file has been generated.".format(filename))
 
